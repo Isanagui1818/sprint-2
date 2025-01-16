a8c50df3739497bf4f1a633b87d154551a51f425
Isanagui <114113194+Isanagui1818@users.noreply.github.com>
2025-01-16 17:12:03 +0100

    Update Script Ejercicios SQL Nivel 2.sql

--- Script Ejercicios SQL Nivel 2.sql
+++ Script Ejercicios SQL Nivel 2.sql
@@ -1,5 +1,4 @@
-/*Exercici 1
-Identifica els cinc dies que es va generar la quantitat més gran d'ingressos a l'empresa per vendes. 
+/*Exercici 1 Identifica els cinc dies que es va generar la quantitat més gran d'ingressos a l'empresa per vendes. 
 Mostra la data de cada transacció juntament amb el total de les vendes. */
 
 select date(timestamp) as fecha, sum(amount) as ventas
@@ -11,8 +10,7 @@ limit 5;
 
 -- he utlizado el filtro declined=0 para quitar las transacciones que no se han efectuado
 
-/*Exercici 2
-Quina és la mitjana de vendes per país? Presenta els resultats ordenats de major a menor mitjà.*/
+/*Exercici 2 Quina és la mitjana de vendes per país? Presenta els resultats ordenats de major a menor mitjà.*/
 
 select country, round(avg(amount),2) as media_compra
 from transactions.company
@@ -24,8 +22,7 @@ order by media_compra desc;
 -- he utlizado el filtro declined=0 para quitar las transacciones que no se han efectuado, pongo round 2 para que solo aparezcan 2 decimales
 
 /*
-Exercici 3
-En la teva empresa, es planteja un nou projecte per a llançar algunes campanyes publicitàries per a fer competència a la companyia "Non Institute". 
+Exercici 3 En la teva empresa, es planteja un nou projecte per a llançar algunes campanyes publicitàries per a fer competència a la companyia "Non Institute". 
 Per a això, et demanen la llista de totes les transaccions realitzades per empreses que estan situades en el mateix país que aquesta companyia.
     Mostra el llistat aplicant JOIN i subconsultes.
     Mostra el llistat aplicant solament subconsultes.
